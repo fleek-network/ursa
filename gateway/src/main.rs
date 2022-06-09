f2fcd90b51b9c79a9d9a938f82869560bda9755e
BotchM <botchm@hotmail.com>
2022-06-09 14:30:49 +0300

    use axum rust

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,12 +1,72 @@
-use warp::Filter;
+use axum::{
+    extract::Extension,
+    http::{uri::Uri, Request, Response},
+    routing::get,
+    Router,
+};
+use axum_server::tls_rustls::RustlsConfig;
+use hyper::{client::HttpConnector, Body};
+use std::{convert::TryFrom, net::SocketAddr};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+type Client = hyper::client::Client<HttpConnector, Body>;
 
 #[tokio::main]
 async fn main() {
-    // Match any request and return hello world!
-    let routes = warp::any().map(|| "Hello, Ursa!");
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "example_tls_rustls=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    warp::serve(routes)
-        // ipv6 + ipv6 any addr
-        .run(([0, 0, 0, 0, 0, 0, 0, 0], 8080))
-        .await;
+    tokio::spawn(server());
+
+    let client = Client::new();
+    let config =
+        RustlsConfig::from_pem_file("self_signed_certs/cert.pem", "self_signed_certs/key.pem")
+            .await
+            .unwrap();
+
+    let app = Router::new()
+        .route("/", get(handler))
+        .layer(Extension(client));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
+    println!("reverse proxy listening on {}", addr);
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler(
+    Extension(client): Extension<Client>,
+    // NOTE: Make sure to put the request extractor last because once the request
+    // is extracted, extensions can't be extracted anymore.
+    mut req: Request<Body>,
+) -> Response<Body> {
+    let path = req.uri().path();
+    let path_query = req
+        .uri()
+        .path_and_query()
+        .map(|v| v.as_str())
+        .unwrap_or(path);
+
+    let uri = format!("http://127.0.0.1:3000{}", path_query);
+
+    *req.uri_mut() = Uri::try_from(uri).unwrap();
+
+    client.request(req).await.unwrap()
+}
+
+async fn server() {
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("server listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
