0cd954ebe041cc0ee7592debe8e5d255be9cafc6
BotchM <botchm@hotmail.com>
2022-08-03 15:37:33 +0300

    add node and rpc firewall rules

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -102,6 +102,18 @@ resource "digitalocean_firewall" "ursa-network" {
   }
 
   inbound_rule {
+    port_range = "4069"
+    protocol   = "tcp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    port_range = "6009"
+    protocol   = "tcp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
     protocol         = "icmp"
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
