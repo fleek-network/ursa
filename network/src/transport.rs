fb920b6d54a6ce6de26185bbd069764eb4508cc9
marslan <muhammadarslan1502@gmail.com>
2022-08-04 00:48:02 -0700

    revert libp2p upgrades

--- network/src/transport.rs
+++ network/src/transport.rs
@@ -15,9 +15,10 @@ use libp2p::{
     identity::Keypair,
     mplex, noise,
     relay::v2::client::Client as RelayClient,
-    tcp::{GenTcpConfig, TcpTransport},
+    tcp::TcpConfig,
     yamux, PeerId, Transport,
 };
+use std::time::Duration;
 
 use crate::config::UrsaConfig;
 
@@ -51,13 +52,17 @@ impl UrsaTransport {
                 SelectUpgrade::new(yamux::YamuxConfig::default(), mplex::MplexConfig::default())
             };
 
-            let tcp = TcpTransport::new(GenTcpConfig::new());
-            let tcp = block_on(DnsConfig::system(tcp)).unwrap();
-
-            tcp.upgrade(upgrade::Version::V1)
+            let tcp = TcpConfig::new()
+                .nodelay(true)
+                .port_reuse(true)
+                // .or_transport(Some(relay.0))
+                .upgrade(upgrade::Version::V1)
                 .authenticate(noise)
                 .multiplex(mplex)
-                .boxed()
+                .timeout(Duration::from_secs(20))
+                .boxed();
+
+            block_on(DnsConfig::system(tcp)).unwrap()
         };
 
         // let quic = {
@@ -76,6 +81,6 @@ impl UrsaTransport {
         //         EitherOutput::Second((peer_id, muxer)) => (peer_id, StreamMuxerBox::new(muxer)),
         //     })
         //     .boxed()
-        tcp
+        tcp.boxed()
     }
 }
