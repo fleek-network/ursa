9260d97e28384aab593ac8dc3489dc5f82809d75
marslan <muhammadarslan1502@gmail.com>
2022-08-04 00:51:57 -0700

    add testnet nodes to bootstrap list

--- network/src/config.rs
+++ network/src/config.rs
@@ -7,6 +7,9 @@ pub const DEFAULT_BOOTSTRAP: [&'static str; 2] = [
     "/ip4/146.190.232.131/tcp/6009/p2p/12D3KooWCT69zoBuR9uNUwUteDCV9QvKWymruLcjd915AQgmsYem",
     // "/ip4/0.0.0.0/tcp/4001udp/4001/quic/p2p/Qm",
     // "/ip4/127.0.0.1/tcp/6009",
+    "/ip4/188.166.115.240/tcp/6009",
+    "/ip4/128.199.32.248/tcp/6009",
+    "/ip4/167.99.33.222/tcp/6009",
 ];
 
 pub const DEFAULT_DB_PATH_STR: &'static str = "ursa_db";
