c590d1c636a6bc2784c00d1a9e05ac9b778f1626
mtavano <chelotavano.udp@gmail.com>
2022-08-04 20:28:09 -0400

    Add service metrics as dep

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,12 +6,14 @@ use async_std::task;
 use db::{rocks::RocksDb, rocks_config::RocksDbConfig};
 use dotenv::dotenv;
 use libp2p::identity::Keypair;
+use service_metrics::{config::MetricsServiceConfig, service::MetricsService};
 use network::UrsaService;
 use rpc_server::{api::NodeNetworkInterface, config::RpcConfig, server::Rpc};
 use store::Store;
 use structopt::StructOpt;
 use tracing::{error, info};
 use ursa::{cli_error_and_die, wait_until_ctrlc, Cli, Subcommand};
+use ursametrics::{Metrics, MetricsServerConfig};
 
 #[async_std::main]
 async fn main() {
@@ -35,6 +37,8 @@ async fn main() {
 
                 // todo(botch): check local for a stored keypair
                 let keypair = Keypair::generate_ed25519();
+
+                // Database connection
                 let config_db_path = config.database_path.as_ref().unwrap().as_path().to_str();
                 let db_path = opts
                     .database_path
@@ -44,9 +48,13 @@ async fn main() {
                 let db = RocksDb::open(db_path, &RocksDbConfig::default())
                     .expect("Opening RocksDB must succeed");
                 let db = Arc::new(db);
-
                 let store = Arc::new(Store::new(Arc::clone(&db)));
-                let service = UrsaService::new(keypair, &config, Arc::clone(&store));
+
+                // Configure metrics service
+                let metrics_service = MetricsService::new();
+
+                // Configure ursa service
+                let service = UrsaService::new(keypair, &config, Arc::clone(&store), metrics_service.clone());
                 let rpc_sender = service.command_sender().clone();
 
                 // Start libp2p service
@@ -55,6 +63,8 @@ async fn main() {
                         error!("[service_task] - {:?}", err);
                     }
                 });
+
+                // Configure rpc server
                 let RpcConfig { rpc_addr, rpc_port } = RpcConfig::default();
                 let port = opts.rpc_port.unwrap_or(rpc_port);
                 let rpc_config = RpcConfig::new(port, rpc_addr);
@@ -69,6 +79,12 @@ async fn main() {
                 let rpc_task = task::spawn(async move {
                     if let Err(err) = rpc.start(rpc_config).await {
                         error!("[rpc_task] - {:?}", err);
+                    }
+                });
+
+                task::spawn(async move {
+                    if let Err(err) = metrics_service.start(&MetricsServiceConfig::default()).await {
+                        error!("[metrics task] - {:?}", err);
                     }
                 });
 
