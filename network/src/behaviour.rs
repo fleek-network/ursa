da3d271afe64f43cb51b0824d67d4d30ef48bdd9
BotchM <botchm@hotmail.com>
2022-08-05 23:43:43 +0300

    Revert "revert libp2p upgrades"
    This reverts commit 3a59ae2cc9e50c6d0239796fbdc17160077b22b3.

--- network/src/behaviour.rs
+++ network/src/behaviour.rs
@@ -109,6 +109,7 @@ pub struct Behaviour<P: StoreParams> {
     identify: Identify,
 
     /// Bitswap for exchanging data between blocks between peers.
+    #[behaviour(ignore)]
     bitswap: Bitswap<P>,
 
     /// Ursa's gossiping protocol for message propagation.
