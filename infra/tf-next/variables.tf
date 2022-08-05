262553ca2a4b602ce2a875c40a610186ce813ae3
vojtech <vojtech.studenka@gmail.com>
2022-08-05 20:20:00 +0200

    Terraform + Github Actions

---
+++ infra/tf-next/variables.tf
@@ -0,0 +1,43 @@
+################
+#   Project    #
+################
+
+variable "project_name" {
+  type        = string
+  default     = "Ursa Testnet"
+  description = "Name of the project in DigitalOcean"
+}
+
+variable "project_description" {
+  type        = string
+  default     = "Ursa testnet resources"
+  description = "Description of the project in DigitalOcean"
+}
+
+variable "project_purpose" {
+  type        = string
+  default     = "Nebula"
+  description = "Purpose of the project in DigitalOcean"
+}
+
+variable "project_stage" {
+  type        = string
+  default     = "Development"
+  description = "Stage of the project in DigitalOcean"
+}
+
+################
+#   Droplet    #
+################
+
+variable "k8s_droplet_size" {
+  type        = string
+  default     = "s-4vcpu-8gb"
+  description = "Default k8s droplet size identifier"
+}
+
+variable "k8s_node_count" {
+  default     = 7
+  type        = number
+  description = "How many testnet nodes to deploy"
+}
