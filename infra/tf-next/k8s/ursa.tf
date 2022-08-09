86904043119033208c6a401dfc80d5ecd11b0de8
vojtech <vojtech.studenka@gmail.com>
2022-08-09 13:31:42 +0200

    chore: node affinity

--- infra/tf-next/k8s/ursa.tf
+++ infra/tf-next/k8s/ursa.tf
@@ -25,7 +25,60 @@ resource "kubernetes_deployment" "ursa_node" {
           image = var.k8s_ursa_docker_image
           name  = "ursa"
           port {
-            container_port = 80
+            container_port = 4069
+          }
+
+          port {
+            container_port = 6009
+            host_port      = 6009
+            protocol       = "TCP"
+          }
+
+          port {
+            container_port = 6009
+            host_port      = 6009
+            protocol       = "UDP"
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 4069
+
+              http_header {
+                name  = "X-Custom-Header"
+                value = "Awesome"
+              }
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+
+        }
+
+        affinity {
+          node_affinity {
+            required_during_scheduling_ignored_during_execution {
+              node_selector_term {
+                match_expressions {
+                  key      = "doks.digitalocean.com/node-pool"
+                  operator = "In"
+                  values   = ["ursa-main"]
+                }
+              }
+            }
           }
         }
       }
