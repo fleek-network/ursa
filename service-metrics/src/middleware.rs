039fb665ad7f3c7a0f5ca3e8f43f0eb8793718e5
mtavano <chelotavano.udp@gmail.com>
2022-08-09 10:30:26 -0400

    Update metrics middleware

--- service-metrics/src/middleware.rs
+++ service-metrics/src/middleware.rs
@@ -1,20 +1,13 @@
 use crate::events;
-use metrics::Label;
 use axum::{extract::MatchedPath, http::Request, middleware::Next, response::IntoResponse};
+use metrics::Label;
 use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
 use std::time::Instant;
 
 const REQUEST_DURATION_LABEL: &'static str = "http_requests_duration_seconds";
 
 pub fn setup_metrics_handler() -> PrometheusHandle {
-    PrometheusBuilder::new()
-        //.set_buckets_for_metric(
-        //Matcher::Full(REQUEST_DURATION_LABEL.to_string()),
-        //&[0.005, 0.01, 0.025, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0],
-        //)
-        //.unwrap()
-        .install_recorder()
-        .unwrap()
+    PrometheusBuilder::new().install_recorder().unwrap()
 }
 
 pub async fn track_metrics<B>(req: Request<B>, next: Next<B>) -> impl IntoResponse {
