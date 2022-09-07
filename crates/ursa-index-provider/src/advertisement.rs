54c58097480647147c420c6b78d6aed414bfcd9b
marslan <muhammadarslan1502@gmail.com>
2022-09-07 00:00:36 -0700

    feat: index provider
    * add advertisement and chunks
    * get cid head for entries field
    * publish ads + add service
    * add indexer api endpoints
    * fix response for head api endpoint
    * start publishing from network
    * publish adds from libp2p
    * announce to indexer
    * cbor conversion for announce message
    * add provider interface

---
+++ crates/ursa-index-provider/src/advertisement.rs
@@ -0,0 +1,162 @@
+use forest_ipld::Ipld;
+use libp2p::{
+    core::{signed_envelope, SignedEnvelope},
+    identity::Keypair,
+    PeerId,
+};
+use multihash::MultihashDigest;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+/// A chunk can hold maximum 400 MB in entries. An entry being 64 bytes
+/// max number of entries 6,250,000
+pub const MAX_ENTRIES: usize = 6250000;
+const AD_SIGNATURE_CODEC: &'static str = "/indexer/ingest/adSignature";
+const AD_SIGNATURE_DOMAIN: &'static str = "indexer";
+
+#[allow(non_snake_case)]
+#[derive(Serialize)]
+struct Metadata {
+    // ProtocolID defines the protocol used for data retrieval.
+    ProtocolID: u128,
+    // Data is specific to the identified protocol, and provides data, or a
+    // link to data, necessary for retrieval.
+    Data: Vec<u8>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Advertisement {
+    /// PreviousID is an optional link to the previous advertisement.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub PreviousID: Option<Ipld>,
+    /// Provider is the peer ID of the host that provides this advertisement.
+    pub Provider: String,
+    /// Addresses is the list of multiaddrs as strings from which the advertised content is retrievable.
+    pub Addresses: Vec<String>,
+    /// Advertisement signature.
+    pub Signature: Ipld,
+    /// Entries with a link to the list of CIDs
+    /// Entries is a link to a data structure that contains the advertised multihashes.
+    pub Entries: Option<Ipld>,
+    /// ContextID is the unique identifier for the collection of advertised multihashes.
+    pub ContextID: Ipld,
+    /// Metadata captures contextual information about how to retrieve the advertised content
+    pub Metadata: Ipld,
+    /// IsRm specifies whether this advertisement represents the content are no longer retrievable fom the provider.
+    pub IsRm: bool,
+}
+impl Advertisement {
+    pub fn new(context_id: Vec<u8>, provider: PeerId, addresses: Vec<String>, is_rm: bool) -> Self {
+        // prtocolid for bitswap
+        let raw_metadata = Metadata {
+            ProtocolID: 0x0900,
+            Data: vec![],
+        };
+        let metadata = bincode::serialize(&raw_metadata).unwrap();
+
+        Self {
+            PreviousID: None,
+            Provider: provider.to_base58(),
+            Addresses: addresses,
+            Signature: Ipld::Bytes(vec![]),
+            Entries: None,
+            Metadata: Ipld::Bytes(metadata),
+            ContextID: Ipld::Bytes(context_id),
+            IsRm: is_rm,
+        }
+    }
+    pub fn sign(&self, signing_key: &Keypair) -> Result<SignedEnvelope, AdSigError> {
+        Ok(SignedEnvelope::new(
+            signing_key,
+            AD_SIGNATURE_DOMAIN.into(),
+            AD_SIGNATURE_CODEC.into(),
+            self.sig_payload()?,
+        )
+        .map_err(AdSigError::SigningError)?)
+    }
+
+    /// computes a signature over all of these fields
+    /// https://github.com/MarcoPolo/http-index-provider-example/blob/6ebda4211c93324405c827b5ffc46c513741efa8/src/advertisement.rs#L49
+    fn sig_payload(&self) -> Result<Vec<u8>, AdSigError> {
+        let mut previous_id_bytes = match &self.PreviousID {
+            Some(Ipld::Link(link)) => Ok(link.to_bytes()),
+            None => Ok(vec![]),
+            _ => Err(AdSigError::InvalidPreviousID),
+        }?;
+
+        let mut entrychunk_link_bytes: Vec<u8> = match &self.Entries {
+            Some(Ipld::Link(link)) => Ok(link.to_bytes()),
+            None => Ok(vec![]),
+            _ => Err(AdSigError::InvalidEntryChunkLink),
+        }?;
+
+        let metadata = match &self.Metadata {
+            Ipld::Bytes(b) => Ok(b),
+            _ => Err(AdSigError::InvalidMetadata),
+        }?;
+
+        let is_rm_payload = if self.IsRm { [1] } else { [0] };
+
+        let mut payload: Vec<u8> = Vec::with_capacity(
+            previous_id_bytes.len()
+                + entrychunk_link_bytes.len()
+                + self.Provider.len()
+                + self.Addresses.iter().map(|s| s.len()).sum::<usize>()
+                + metadata.len()
+                + is_rm_payload.len(),
+        );
+
+        payload.append(&mut previous_id_bytes);
+        payload.append(&mut entrychunk_link_bytes);
+        payload.extend_from_slice(&self.Provider.as_bytes());
+        self.Addresses
+            .iter()
+            .for_each(|s| payload.extend_from_slice(&s.as_bytes()));
+        payload.extend_from_slice(metadata);
+        payload.extend_from_slice(&is_rm_payload);
+
+        Ok(multihash::Code::Sha2_256.digest(&payload).to_bytes())
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum AdSigError {
+    #[error("Invalid Previous ID")]
+    InvalidPreviousID,
+    #[error("Invalid Entry Chunk Link")]
+    InvalidEntryChunkLink,
+    #[error("Invalid Metadata")]
+    InvalidMetadata,
+    #[error("Missing Signature")]
+    MissingSig,
+    #[error("Failed to sign advertisement: {0}")]
+    SigningError(libp2p::identity::error::SigningError),
+    #[error("Failed to decode sig: {0}")]
+    DecodingError(signed_envelope::DecodingError),
+    #[error("Failed to read signed payload: {0}")]
+    ReadPayloadError(signed_envelope::ReadPayloadError),
+    #[error("Payload did not match expected")]
+    PayloadDidNotMatch,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Deserialize, Debug)]
+/// EntryChunk captures a chunk in a chain of entries that collectively contain the multihashes
+/// advertised by an Advertisement.
+pub struct EntryChunk {
+    /// Entries represent the list of multihashes in this chunk.
+    Entries: Vec<Ipld>,
+    /// Next is an optional link to the next entry chunk.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    Next: Option<Ipld>,
+}
+
+impl EntryChunk {
+    pub fn new(entries: Vec<Ipld>, next: Option<Ipld>) -> Self {
+        Self {
+            Entries: entries,
+            Next: next,
+        }
+    }
+}
---
+++ crates/ursa-index-provider/src/signed_head.rs
@@ -0,0 +1,139 @@
+// https://github.com/MarcoPolo/http-index-provider-example/blob/main/src/signed_head.rs
+
+use base64;
+use cid::Cid;
+use libp2p::{
+    core::{identity::Keypair, PublicKey},
+    identity::error::SigningError,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::Map;
+use serde_with::serde_as;
+use thiserror::Error;
+
+#[serde_as]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SignedHead {
+    #[serde_as(as = "CidAsMap")]
+    head: Cid,
+    #[serde_as(as = "BytesAsMap")]
+    sig: Vec<u8>,
+    #[serde_as(as = "BytesAsMap")]
+    pubkey: Vec<u8>,
+}
+
+#[derive(Debug, Error)]
+pub enum SignedHeadError {
+    #[error("Invalid signature")]
+    InvalidSignature,
+    #[error("Invalid public key")]
+    InvalidPublicKey,
+}
+
+impl SignedHead {
+    pub fn new(signing_key: &Keypair, cid: Cid) -> Result<Self, SigningError> {
+        let sig = signing_key.sign(&cid.to_bytes())?;
+        Ok(SignedHead {
+            head: cid,
+            pubkey: signing_key.public().to_protobuf_encoding(),
+            sig,
+        })
+    }
+
+    pub fn open(self) -> Result<(PublicKey, Cid), SignedHeadError> {
+        let pk = PublicKey::from_protobuf_encoding(&self.pubkey)
+            .map_err(|_| SignedHeadError::InvalidPublicKey)?;
+        let valid = pk.verify(&self.head.to_bytes(), &self.sig);
+        if !valid {
+            return Err(SignedHeadError::InvalidSignature);
+        }
+
+        Ok((pk, self.head))
+    }
+}
+
+serde_with::serde_conv!(BytesAsMap, Vec<u8>, from_bytes_to_map, from_map_to_bytes);
+fn from_bytes_to_map(bytes: &Vec<u8>) -> Map<String, serde_json::Value> {
+    let mut m = Map::new();
+    let mut bytes_map = Map::new();
+    bytes_map.insert("bytes".into(), base64::encode(bytes).into());
+    m.insert("/".into(), bytes_map.into());
+    m
+}
+
+fn from_map_to_bytes(
+    value: Map<String, serde_json::Value>,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let bytes_str = value
+        .get("/")
+        .ok_or("Missing link key")?
+        .get("bytes")
+        .ok_or("Missing bytes key")?
+        .as_str()
+        .ok_or("missing bytes string")?;
+
+    Ok(base64::decode(bytes_str).map_err(|e| format!("{}", e))?)
+}
+
+serde_with::serde_conv!(CidAsMap, Cid, from_cid_to_map, from_map_to_cid);
+fn from_cid_to_map(cid: &Cid) -> serde_json::Map<String, serde_json::Value> {
+    let mut map = Map::new();
+    map.insert("/".to_string(), cid.to_string().into());
+    map
+}
+
+fn from_map_to_cid(value: serde_json::Map<String, serde_json::Value>) -> Result<Cid, cid::Error> {
+    let cid_str = value
+        .get("/")
+        .ok_or(cid::Error::Io(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Cid entry is missing",
+        )))?
+        .as_str()
+        .ok_or(cid::Error::Io(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Cid str is missing",
+        )))?;
+
+    let cid = Cid::try_from(cid_str)?;
+
+    Ok(cid)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_decode_signed_msg() {
+        // From Go implementation
+        let signed_msg = r#"{"head":{"/":"bafybeicyhbhhklw3kdwgrxmf67mhkgjbsjauphsvrzywav63kn7bkpmqfa"},"pubkey":{"/":{"bytes":"CAESIJSklColz5Jq+bVsKPQpxmEwo9avM7y/vVkYSDttBWLI"}},"sig":{"/":{"bytes":"1S4p2vHPXobyPnspQWkCHMjf2n5qQCMb+OehDjUnQbRil3qf95g87VNcIxl6hr66zmhBeJ7h+Y6UnUUhnUMZAQ"}}}"#;
+
+        let signed_head: SignedHead = serde_json::from_str(signed_msg).expect("deser failed");
+        let signed_head_encoded = serde_json::to_string(&signed_head).expect("ser failed");
+        // Round trip to test
+        let signed_head: SignedHead =
+            serde_json::from_str(&signed_head_encoded).expect("round trip failed");
+
+        let (pk, head) = signed_head.open().expect("failed to open signed_head");
+
+        println!("{:?}", head);
+        println!("{:?}", pk);
+    }
+
+    #[test]
+    fn test_sign_head() {
+        let kp = Keypair::generate_ed25519();
+        let cid = Cid::try_from("bafybeicyhbhhklw3kdwgrxmf67mhkgjbsjauphsvrzywav63kn7bkpmqfa")
+            .expect("failed to parse cid");
+        let signed_head = SignedHead::new(&kp, cid).expect("failed to sign head");
+        let signed_head_encoded = serde_json::to_string(&signed_head).expect("ser failed");
+        let signed_head: SignedHead =
+            serde_json::from_str(&signed_head_encoded).expect("deser failed");
+
+        let (pk, head) = signed_head.open().expect("failed to open signed_head");
+
+        assert_eq!(head, cid);
+        assert_eq!(pk, kp.public());
+    }
+}
