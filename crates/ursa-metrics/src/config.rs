527056fbe4333d384bd7d37abc01745425c983eb
ozwaldorf <self@ossian.dev>
2022-12-01 23:15:30 -0500

    chore: remove metrics api_path from config

--- crates/ursa-metrics/src/config.rs
+++ crates/ursa-metrics/src/config.rs
@@ -1,15 +1,12 @@
 use serde::{Deserialize, Serialize};
 
 const DEFAULT_METRICS_PORT: u16 = 4070;
-const DEFAULT_API_PATH: &str = "/metrics";
 const DEFAULT_AGENT: &str = "ursa/*";
 
 #[derive(Serialize, Deserialize)]
 pub struct MetricsServiceConfig {
     /// Optional. Port to run metrics server. Default port 4070.
     pub port: u16,
-    /// Options. Path to metrics. Default /metrics
-    pub api_path: String,
     /// Ursa client version
     pub agent: String,
 }
@@ -18,7 +15,6 @@ impl Default for MetricsServiceConfig {
     fn default() -> Self {
         Self {
             port: DEFAULT_METRICS_PORT,
-            api_path: DEFAULT_API_PATH.to_string(),
             agent: DEFAULT_AGENT.to_string(),
         }
     }
