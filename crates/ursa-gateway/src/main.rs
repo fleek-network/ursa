40ef9aaff8e5cb2edbf445cf1d50f08edfd4fa50
BotchM <botchm@hotmail.com>
2022-12-05 23:05:25 -0800

    revert gateway changes

--- crates/ursa-gateway/src/main.rs
+++ crates/ursa-gateway/src/main.rs
@@ -1,3 +1,6 @@
+mod config;
+
+use crate::config::GatewayConfig;
 use axum::{
     extract::Extension,
     http::{uri::Uri, Request, Response},
@@ -7,44 +10,46 @@ use axum::{
 use axum_server::tls_rustls::RustlsConfig;
 use hyper::{client::HttpConnector, Body};
 use std::{convert::TryFrom, net::SocketAddr};
-use tokio::task;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing::info;
 
 type Client = hyper::client::Client<HttpConnector, Body>;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "example_tls_rustls=debug".into()),
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    tracing_subscriber::fmt::init();
 
-    task::spawn(server());
+    tokio::spawn(server(GatewayConfig::default()));
 
-    let client = Client::new();
-    let config =
-        RustlsConfig::from_pem_file("self_signed_certs/cert.pem", "self_signed_certs/key.pem")
+    let config = GatewayConfig::default();
+
+    let rustls_config =
+        RustlsConfig::from_pem_file(config.cert_config.cert_path, config.cert_config.key_path)
             .await
             .unwrap();
 
     let app = Router::new()
         .route("/", get(handler))
-        .layer(Extension(client));
+        .layer(Extension((Client::new(), GatewayConfig::default())));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
-    println!("reverse proxy listening on {}", addr);
-    axum_server::bind_rustls(addr, config)
+    let addr = SocketAddr::from((
+        config
+            .reverse_proxy
+            .addr
+            .parse::<std::net::Ipv4Addr>()
+            .unwrap(),
+        config.reverse_proxy.port,
+    ));
+
+    info!("reverse proxy listening on {}", addr);
+
+    axum_server::bind_rustls(addr, rustls_config)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
 async fn handler(
-    Extension(client): Extension<Client>,
-    // NOTE: Make sure to put the request extractor last because once the request
-    // is extracted, extensions can't be extracted anymore.
+    Extension((client, config)): Extension<(Client, GatewayConfig)>,
     mut req: Request<Body>,
 ) -> Response<Body> {
     let path = req.uri().path();
@@ -54,18 +59,27 @@ async fn handler(
         .map(|v| v.as_str())
         .unwrap_or(path);
 
-    let uri = format!("http://127.0.0.1:3000{}", path_query);
+    let uri = format!(
+        "http://{}:{}{}",
+        config.server.addr, config.server.port, path_query
+    );
 
     *req.uri_mut() = Uri::try_from(uri).unwrap();
 
     client.request(req).await.unwrap()
 }
 
-async fn server() {
+async fn server(config: GatewayConfig) {
+    // TODO: forward node
     let app = Router::new().route("/", get(|| async { "Hello, world!" }));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("server listening on {}", addr);
+    let addr = SocketAddr::from((
+        config.server.addr.parse::<std::net::Ipv4Addr>().unwrap(),
+        config.server.port,
+    ));
+
+    info!("server listening on {}", addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
