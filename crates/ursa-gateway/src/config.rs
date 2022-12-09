388eabc534683f446e86a26b8361c11324a5c7ac
Scott Dn <scott.dn91@gmail.com>
2022-12-10 02:12:56 +0700

    chore: update config

--- crates/ursa-gateway/src/config.rs
+++ crates/ursa-gateway/src/config.rs
@@ -1,16 +1,48 @@
-#[derive(Clone)]
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
+use std::{
+    fs::{create_dir_all, read_to_string, File},
+    io::Write,
+    path::PathBuf,
+};
+
+pub const DEFAULT_URSA_GATEWAY_PATH: &str = ".ursa/gateway/";
+pub const DEFAULT_URSA_GATEWAY_CONFIG_PATH: &str = ".ursa/gateway/config.toml";
+
+pub fn init_config() {
+    let path = PathBuf::from(env!("HOME")).join(DEFAULT_URSA_GATEWAY_CONFIG_PATH);
+    if !path.exists() {
+        info!("init config path at: {:?}", path);
+        create_dir_all(path.parent().expect("create parent config path"))
+            .expect("couldn't create parent config path");
+        let gateway_config = GatewayConfig::default();
+        let mut file = File::create(path).expect("create config path");
+        let toml = toml::to_string(&gateway_config).expect("toml serialization");
+        file.write_all(toml.as_bytes()).expect("problem write file");
+    }
+}
+
+pub fn load_config() -> GatewayConfig {
+    let path = PathBuf::from(env!("HOME")).join(DEFAULT_URSA_GATEWAY_CONFIG_PATH);
+    info!("load config path at: {:?}", path);
+    let toml = read_to_string(path).expect("problem read file");
+    toml::from_str(&toml).expect("load config failed")
+}
+
+#[derive(Clone, Deserialize, Serialize)]
 pub struct GatewayConfig {
     pub server: ServerConfig,
     pub cert_config: CertConfig,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct CertConfig {
     pub cert_path: String,
     pub key_path: String,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct ServerConfig {
     pub port: u16,
     pub addr: String,
@@ -21,12 +53,21 @@ impl Default for GatewayConfig {
         Self {
             server: ServerConfig {
                 addr: "0.0.0.0".to_string(),
-                port: 3000,
+                port: 80,
             },
             cert_config: CertConfig {
-                // TODO: move out to better location.
-                cert_path: "./crates/ursa-gateway/self_signed_certs/cert.pem".to_string(),
-                key_path: "./crates/ursa-gateway/self_signed_certs/key.pem".to_string(),
+                cert_path: PathBuf::from(env!("HOME"))
+                    .join(DEFAULT_URSA_GATEWAY_PATH)
+                    .join("cert.pem")
+                    .to_str()
+                    .expect("bad directory")
+                    .into(),
+                key_path: PathBuf::from(env!("HOME"))
+                    .join(DEFAULT_URSA_GATEWAY_PATH)
+                    .join("key.pem")
+                    .to_str()
+                    .expect("bad directory")
+                    .into(),
             },
         }
     }
