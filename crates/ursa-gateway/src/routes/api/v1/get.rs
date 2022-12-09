89f410802977341978cbeab20d5352fdbc5385b1
Scott Dn <scott.dn91@gmail.com>
2022-12-10 02:13:30 +0700

    chore: update route

--- crates/ursa-gateway/src/routes/api/v1/get.rs
+++ crates/ursa-gateway/src/routes/api/v1/get.rs
@@ -1,12 +1,6 @@
-use axum::{extract::Query, response::IntoResponse, Json};
+use axum::{extract::Path, response::IntoResponse};
 use hyper::StatusCode;
-use serde_json::json;
-use ursa_rpc_client::functions::get_block;
-use ursa_rpc_server::api::NetworkGetParams;
 
-pub async fn get_block_handler(Query(params): Query<NetworkGetParams>) -> impl IntoResponse {
-    match get_block(params).await {
-        Ok(res) => (StatusCode::OK, Json(json!(res))),
-        Err(err) => super::handle_error(err),
-    }
+pub async fn get_block_handler(Path(cid): Path<String>) -> impl IntoResponse {
+    (StatusCode::OK, cid)
 }
