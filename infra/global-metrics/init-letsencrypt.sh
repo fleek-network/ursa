9a39238640084956888e292ab32acdfc794ac704
ozwaldorf <self@ossian.dev>
2022-12-10 04:46:31 -0500

    fix(global-metrics): nginx, use real letsencrypt, deploy scripts, open ufw ports

--- infra/global-metrics/init-letsencrypt.sh
+++ infra/global-metrics/init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(tracker.ursa.earth prometheus.ursa.earth grafana.ursa.earth)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="major@ursa.earth" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
