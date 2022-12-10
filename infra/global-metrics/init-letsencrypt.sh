664b1410f559b36bd9f5432a83e5f2714abcae5a
ozwaldorf <self@ossian.dev>
2022-12-10 15:47:08 -0500

    fix: automatic ssl cert generation, default grafana dashboard path

--- infra/global-metrics/init-letsencrypt.sh
+++ infra/global-metrics/init-letsencrypt.sh
@@ -11,13 +11,6 @@ data_path="./data/certbot"
 email="major@ursa.earth" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
-
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
@@ -70,6 +63,7 @@ docker-compose run --rm --entrypoint "\
     $domain_args \
     --rsa-key-size $rsa_key_size \
     --agree-tos \
+    -n \
     --force-renewal" certbot
 echo
 
