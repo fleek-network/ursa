76110bc71b57ea64240033f5fd0d92d28d2bc64f
Scott Dn <scott.dn91@gmail.com>
2022-12-13 19:45:47 +0700

    feat: IndexderResponse model

---
+++ crates/ursa-gateway/src/indexer/model.rs
@@ -0,0 +1,33 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct IndexerResponse {
+    #[serde(rename = "MultihashResults")]
+    multihash_results: Vec<MultihashResult>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct MultihashResult {
+    #[serde(rename = "Multihash")]
+    multihash: String,
+    #[serde(rename = "ProviderResults")]
+    provider_results: Vec<ProviderResult>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ProviderResult {
+    #[serde(rename = "ContextID")]
+    context_id: String,
+    #[serde(rename = "Metadata")]
+    metadata: String,
+    #[serde(rename = "Provider")]
+    provider: AddrInfo,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct AddrInfo {
+    #[serde(rename = "ID")]
+    id: String,
+    #[serde(rename = "Addrs")]
+    addrs: Vec<multiaddr::Multiaddr>,
+}
