1d5b6da9231e318b3eec6fb3bcd17816f27a9d70
Scott Dn <scott.dn91@gmail.com>
2022-12-13 19:49:02 +0700

    feat: query indexer

--- crates/ursa-gateway/src/server/route/api/v1/get.rs
+++ crates/ursa-gateway/src/server/route/api/v1/get.rs
@@ -1,18 +1,19 @@
 use axum::{extract::Path, response::IntoResponse, Extension, Json};
 use cid::Cid;
-use hyper::{StatusCode, Uri};
-use serde_json::json;
-use std::str::FromStr;
-use tracing::debug;
+use hyper::{body, StatusCode, Uri};
+use serde_json::{from_slice, json};
+use std::{str::FromStr, sync::Arc};
+use tracing::{debug, error};
 
 use crate::{
     config::GatewayConfig,
+    indexer::model::IndexerResponse,
     server::{model::HttpResponse, Client},
 };
 
 pub async fn get_block_handler(
     Path(cid): Path<String>,
-    Extension((client, config)): Extension<(Client, GatewayConfig)>,
+    Extension((client, config)): Extension<(Client, Arc<GatewayConfig>)>,
 ) -> impl IntoResponse {
     if Cid::from_str(&cid).is_err() {
         return (
@@ -27,7 +28,7 @@ pub async fn get_block_handler(
     let uri = match endpoint.parse::<Uri>() {
         Ok(uri) => uri,
         Err(e) => {
-            debug!("error parsed uri: {}\n{}", endpoint, e);
+            error!("error parsed uri: {}\n{}", endpoint, e);
             return (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 Json(json!(HttpResponse {
@@ -37,10 +38,10 @@ pub async fn get_block_handler(
             );
         }
     };
-    let _resp = match client.get(uri).await {
+    let resp = match client.get(uri).await {
         Ok(resp) => resp,
         Err(e) => {
-            debug!("error requested uri: {}\n{}", endpoint, e);
+            error!("error requested uri: {}\n{}", endpoint, e);
             return (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 Json(json!(HttpResponse {
@@ -50,5 +51,41 @@ pub async fn get_block_handler(
             );
         }
     };
-    (StatusCode::OK, Json(json!("")))
+    let bytes = match body::to_bytes(resp.into_body()).await {
+        Ok(bytes) => bytes,
+        Err(e) => {
+            error!("error read data from upstream: {}\n{}", endpoint, e);
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!(HttpResponse {
+                    message: Some(format!("error read data from upstream: {}", endpoint)),
+                    data: None
+                })),
+            );
+        }
+    };
+    let indexer_response: IndexerResponse = match from_slice(&bytes) {
+        Ok(indexer_response) => indexer_response,
+        Err(e) => {
+            error!(
+                "error parsed indexer response from upstream: {}\n{}",
+                endpoint, e
+            );
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!(HttpResponse {
+                    message: Some(format!(
+                        "error parsed indexer response from upstream: {}",
+                        endpoint
+                    )),
+                    data: None
+                })),
+            );
+        }
+    };
+    debug!(
+        "received indexer response for {cid}:\n{:?}",
+        indexer_response
+    );
+    (StatusCode::OK, Json(json!(indexer_response)))
 }
