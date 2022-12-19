9a9d473784d8bc345255a1a4e23df95f56f9f4d9
Scott Dn <scott.dn91@gmail.com>
2022-12-20 06:57:17 +0700

    feat: handle cap exceeded

--- crates/ursa-gateway/src/cache/lru.rs
+++ crates/ursa-gateway/src/cache/lru.rs
@@ -1,25 +1,21 @@
 use std::{cell::RefCell, collections::HashMap, hash::Hash, rc::Rc};
 
-#[derive(Debug, PartialEq)]
 struct Node<T> {
     next: RefCell<Rc<Dll<T>>>,
     prev: RefCell<Rc<Dll<T>>>,
     data: T,
 }
 
-#[derive(Debug, PartialEq)]
 enum Dll<K> {
     _Node(Node<K>),
     _Nil,
 }
 
-#[derive(Debug, PartialEq)]
 struct Data<K, V> {
     value: V,
     dll: Rc<Dll<K>>,
 }
 
-#[derive(Debug, PartialEq)]
 struct Lru<K: Eq + Hash, V> {
     store: HashMap<K, Data<K, V>>,
     head: Rc<Dll<K>>,
@@ -45,13 +41,34 @@ where
         }
     }
 
+    fn _get_first_key(&self) -> Option<K> {
+        if let Dll::_Node(node) = self.head.as_ref() {
+            Some(node.data.clone())
+        } else {
+            None
+        }
+    }
+
+    fn _contains(&self, k: &K) -> bool {
+        self.store.contains_key(k)
+    }
+
     pub fn _get(&self, k: &K) -> Option<&V> {
         self.store.get(k).map(|data| &data.value)
     }
 
     pub fn _insert(&mut self, k: K, v: V) {
-        // if self.cap <= self.store.len() as u64 {}
-        // TODO: handle duplicate
+        if self._contains(&k) {
+            return;
+        }
+        if let Some(cap) = self.cap {
+            if cap <= self.store.len() {
+                let first_key = self
+                    ._get_first_key()
+                    .expect("[LRU]: Failed to get the first key while deleting.");
+                self._remove(&first_key);
+            }
+        }
         let new_tail = Rc::new(Dll::_Node(Node {
             next: RefCell::new(Rc::new(Dll::_Nil)),
             prev: RefCell::new(self.tail.clone()),
@@ -135,15 +152,10 @@ mod tests {
 
         #[test]
         fn new() {
-            assert_eq!(
-                Lru::<u8, u8>::_new(None),
-                Lru {
-                    store: HashMap::new(),
-                    head: Rc::new(Dll::_Nil),
-                    tail: Rc::new(Dll::_Nil),
-                    cap: None
-                }
-            );
+            let lru = Lru::<&str, u8>::_new(None);
+            assert_eq!(lru.cap, None);
+            assert!(lru.k_from_head().is_empty());
+            assert!(lru.k_from_tail().is_empty());
         }
 
         #[test]
@@ -198,15 +210,8 @@ mod tests {
             let mut lru = Lru::_new(None);
             lru._insert("a", 1);
             lru._remove(&"a");
-            assert_eq!(
-                lru,
-                Lru {
-                    store: HashMap::new(),
-                    head: Rc::new(Dll::_Nil),
-                    tail: Rc::new(Dll::_Nil),
-                    cap: None
-                }
-            );
+            assert!(lru.k_from_head().is_empty());
+            assert!(lru.k_from_tail().is_empty());
         }
 
         #[test]
@@ -255,6 +260,15 @@ mod tests {
         }
 
         #[test]
+        fn insert_duplicate() {
+            let mut lru = Lru::_new(None);
+            lru._insert("a", 1);
+            lru._insert("a", 1);
+            assert_eq!(lru.k_from_head(), ["a"]);
+            assert_eq!(lru.k_from_tail(), ["a"]);
+        }
+
+        #[test]
         fn insert_one() {
             let mut lru = Lru::_new(None);
             lru._insert("a", 1);
@@ -279,6 +293,37 @@ mod tests {
             lru._insert("c", 3);
             assert_eq!(lru.k_from_head(), ["a", "b", "c"]);
             assert_eq!(lru.k_from_tail(), ["c", "b", "a"]);
+        }
+    }
+
+    mod cap {
+        use super::*;
+
+        #[test]
+        fn new() {
+            let lru = Lru::<&str, u8>::_new(Some(3));
+            assert_eq!(lru.cap, Some(3));
+            assert!(lru.k_from_head().is_empty());
+            assert!(lru.k_from_tail().is_empty());
+        }
+
+        #[test]
+        fn insert_exceed_cap() {
+            let mut lru = Lru::_new(Some(3));
+            lru._insert("a", 1);
+            lru._insert("b", 2);
+            lru._insert("c", 3);
+            lru._insert("d", 4);
+            assert_eq!(lru.k_from_head(), ["b", "c", "d"]);
+            assert_eq!(lru.k_from_tail(), ["d", "c", "b"]);
+            let res = lru._get(&"a");
+            assert_eq!(res, None);
+            let res = lru._get(&"b");
+            assert_eq!(res, Some(&2));
+            let res = lru._get(&"c");
+            assert_eq!(res, Some(&3));
+            let res = lru._get(&"d");
+            assert_eq!(res, Some(&4));
         }
     }
 }
