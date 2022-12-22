270d8012ba3e8bd2f7116178a53b9decdfb8ea22
b0xtch <botchm@hotmail.com>
2022-12-22 00:18:09 -0800

    update variables and cert issuer

--- infra/tf-next/k8s/variables.tf
+++ infra/tf-next/k8s/variables.tf
@@ -1,26 +1,31 @@
-variable "ghcr_registry_username" {
-}
-variable "ghcr_registry_password" {
-}
+################
+#   Project    #
+################
 
 variable "do_project_id" {
-}
-variable "do_region" {
-}
-
-variable "registry_server" {
-  default = "ghcr.io"
+  type        = string
+  description = "The digital ocean project id"
 }
 
-variable "k8s_ursa_docker_image" {
-  default = "nginx"
-}
-
-variable "k8s_droplet_size" {
+variable "default_droplet_size" {
   type        = string
   default     = "s-2vcpu-4gb"
   description = "Default k8s droplet size identifier"
 }
+
+variable "top_level_domains" {
+  type        = list(string)
+  description = "Top level domains to create records and pods for"
+}
+
+variable "lets_encrypt_env" {
+  type        = string
+  description = "Lets Encrypt `staging` or `prod`"
+}
+
+################
+#   Cluster    #
+################
 
 variable "k8s_min_node_count" {
   default     = 3
@@ -32,4 +37,36 @@ variable "k8s_max_node_count" {
   default     = 6
   type        = number
   description = "How many testnet nodes to deploy"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "The name of the kubernetes cluster to create"
+}
+
+variable "cluster_version" {
+  type        = string
+  description = "The version of the kubernetes cluster to create"
+}
+
+variable "region" {
+  type        = string
+  default     = "ams3"
+  description = "The digital ocean region slug for where to create resources"
+}
+
+###########
+# Images  #
+###########
+variable "k8s_ursa_docker_image" {
+  default     = "nginx"
+}
+
+###########
+# ghcr.io #
+###########
+
+variable "registry_server" {
+  type        = string
+  default     = "ghcr.io"
 }
