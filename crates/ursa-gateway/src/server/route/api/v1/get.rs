90f2c181b4bae1c2be8e7531a7069f5d6e166e7a
Scott Dn <scott.dn91@gmail.com>
2022-12-29 19:44:43 +0700

    chore: refactor api

--- crates/ursa-gateway/src/server/route/api/v1/get.rs
+++ crates/ursa-gateway/src/server/route/api/v1/get.rs
@@ -1,7 +1,6 @@
 use std::{str::FromStr, sync::Arc};
 
 use axum::{
-    body::StreamBody,
     extract::Path,
     http::{header, StatusCode},
     response::{IntoResponse, Response},
@@ -10,13 +9,7 @@ use axum::{
 };
 use cid::Cid;
 use serde_json::{json, Value};
-use tokio::{
-    io::{duplex, AsyncWriteExt},
-    sync::RwLock,
-    task::spawn,
-};
-use tokio_util::io::ReaderStream;
-use tracing::error;
+use tokio::sync::RwLock;
 
 use crate::{server::model::HttpResponse, worker::cache::ServerCache};
 
@@ -33,28 +26,20 @@ pub async fn get_block_handler<Cache: ServerCache>(
     };
 
     match cache.read().await.get_announce(&cid).await {
-        Ok(data) => {
-            let (mut w, r) = duplex(2 * 1024 * 1024); // 2MiB
-            spawn(async move {
-                if let Err(e) = w.write_all(data.as_slice()).await {
-                    error!("Failed to write stream for {e:?}");
-                }
-            });
-            (
-                [
-                    (
-                        header::CONTENT_TYPE,
-                        "application/vnd.curl.car; charset=utf-8",
-                    ),
-                    (
-                        header::CONTENT_DISPOSITION,
-                        &format!("attachment; filename=\"{cid}.car\""),
-                    ),
-                ],
-                StreamBody::new(ReaderStream::new(r)),
-            )
-                .into_response()
-        }
+        Ok(stream) => (
+            [
+                (
+                    header::CONTENT_TYPE,
+                    "application/vnd.curl.car; charset=utf-8",
+                ),
+                (
+                    header::CONTENT_DISPOSITION,
+                    &format!("attachment; filename=\"{cid}.car\""),
+                ),
+            ],
+            stream,
+        )
+            .into_response(),
         Err(message) =>
             error_handler(StatusCode::INTERNAL_SERVER_ERROR, message.to_string()).into_response(),
     }
