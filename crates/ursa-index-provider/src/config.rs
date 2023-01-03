97fa8e0ce8e2f822432aabb43656ed7aef9cf607
ozwaldorf <self@ossian.dev>
2023-01-03 00:57:06 -0500

    chore: simplify default index provider db path

--- crates/ursa-index-provider/src/config.rs
+++ crates/ursa-index-provider/src/config.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
-const DEFAULT_DB_PATH_STR: &str = ".ursa/data/index_provider_db";
+const DEFAULT_DB_PATH_STR: &str = "~/.ursa/data/index_provider_db";
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct ProviderConfig {
@@ -18,7 +18,7 @@ impl Default for ProviderConfig {
         Self {
             domain: "".to_string(),
             indexer_url: "https://dev.cid.contact".to_string(),
-            database_path: format!("~/{DEFAULT_DB_PATH_STR}").into(),
+            database_path: DEFAULT_DB_PATH_STR.into(),
         }
     }
 }
