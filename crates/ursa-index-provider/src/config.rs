91818d983c957cecd7acfa5331556c1329838b1d
ozwaldorf <self@ossian.dev>
2023-01-05 13:37:57 -0500

    feat: auto-upgraded configs using defaults + log the differences (#242)
    * feat: auto-upgraded configs using defaults + log the differences
    - Provide deserialization defaults to automatically populate missing values
    - Calculate unified diff on the toml
    - If changes were made, log the diff and save the new config to file

--- crates/ursa-index-provider/src/config.rs
+++ crates/ursa-index-provider/src/config.rs
@@ -1,24 +1,34 @@
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
-const DEFAULT_DB_PATH_STR: &str = "~/.ursa/data/index_provider_db";
-
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct ProviderConfig {
-    /// a domain where provider is listening dns/test-node.provider.ursa.earth
+    /// a domain where the node is listening, eg. `dns/test-node.ursa.earth`
+    #[serde(default)]
     pub domain: String,
-    /// indexer url to point to e.g. https://dev.cid.contact
+    /// indexer url to point to, eg. https://dev.cid.contact
+    #[serde(default = "ProviderConfig::default_indexer_url")]
     pub indexer_url: String,
     /// database_path for index provider db
+    #[serde(default = "ProviderConfig::default_database_path")]
     pub database_path: PathBuf,
+}
+
+impl ProviderConfig {
+    fn default_database_path() -> PathBuf {
+        "~/.ursa/data/index_provider_db".into()
+    }
+    fn default_indexer_url() -> String {
+        "https://dev.cid.contact".to_string()
+    }
 }
 
 impl Default for ProviderConfig {
     fn default() -> Self {
         Self {
             domain: "".to_string(),
-            indexer_url: "https://dev.cid.contact".to_string(),
-            database_path: DEFAULT_DB_PATH_STR.into(),
+            indexer_url: Self::default_indexer_url(),
+            database_path: Self::default_database_path(),
         }
     }
 }
--- crates/ursa/src/config.rs
+++ crates/ursa/src/config.rs
@@ -1,58 +1,73 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result};
+use imara_diff::{intern::InternedInput, sink::Counter, Algorithm, UnifiedDiffBuilder};
 use serde::{Deserialize, Serialize};
+use std::fmt::Display;
 use std::{
     fs::{create_dir_all, File},
     io::{Read, Write},
-    path::{Path, PathBuf},
+    path::PathBuf,
 };
-use toml::from_str;
-use tracing::info;
+use tracing::{info, warn};
 use ursa_index_provider::config::ProviderConfig;
 use ursa_network::NetworkConfig;
 use ursa_rpc_service::config::ServerConfig;
 
 pub const DEFAULT_CONFIG_PATH_STR: &str = ".ursa/config.toml";
 
-/// Load an UrsaConfig from a path. If the path does not exist, a default config will be created and returned.
-pub fn load_config(path: &PathBuf) -> Result<UrsaConfig> {
-    info!("Loading config from: {:?}", path);
-    if path.exists() {
-        let toml = read_file_to_string(path).context(format!(
-            "Failed to read config file {}",
-            path.to_string_lossy()
-        ))?;
-        from_str(&toml).map_err(|e| anyhow!("Failed to parse config toml: {}", e))
-    } else {
-        // Config missing, create and return default config at path
-        let ursa_config = UrsaConfig::default();
-        let toml = toml::to_string(&ursa_config).unwrap();
-        create_dir_all(path.parent().unwrap()).context(format!(
-            "Failed to create default config directory: {}",
-            path.to_string_lossy()
-        ))?;
-        let mut file = File::create(path).context(format!(
-            "Failed to create default config file: {}",
-            path.to_string_lossy()
-        ))?;
-        file.write_all(toml.as_bytes()).context(format!(
-            "Failed to write to default config file: {}",
-            path.to_string_lossy()
-        ))?;
-        Ok(ursa_config)
-    }
-}
-
 #[derive(Default, Serialize, Deserialize, Debug)]
 pub struct UrsaConfig {
+    #[serde(default)]
     pub network_config: NetworkConfig,
+    #[serde(default)]
     pub provider_config: ProviderConfig,
+    #[serde(default)]
     pub server_config: ServerConfig,
 }
 
-/// Read file as a `String`.
-pub fn read_file_to_string(path: &Path) -> Result<String> {
-    let mut file = File::open(path)?;
-    let mut string = String::new();
-    file.read_to_string(&mut string)?;
-    Ok(string)
+impl UrsaConfig {
+    /// Load an UrsaConfig from a given path, or create a default one if not found.
+    pub fn load_or_default(path: &PathBuf) -> Result<UrsaConfig> {
+        info!("Loading config from: {:?}", path);
+        if path.exists() {
+            // read file
+            let mut file = File::open(path)?;
+            let mut raw = String::new();
+            file.read_to_string(&mut raw)?;
+
+            let config: UrsaConfig = toml::from_str(&raw).context("Failed to parse config file")?;
+
+            // check if we modified the config at all
+            let config_str = toml::to_string(&config)?;
+            let input = InternedInput::new(raw.as_str(), config_str.as_str());
+            let diff = imara_diff::diff(
+                Algorithm::Histogram,
+                &input,
+                Counter::new(UnifiedDiffBuilder::new(&input)),
+            );
+            if diff.total() > 0 {
+                warn!(
+                    "Config `{path:?}` was automatically modified and saved to disk:\n\n{}",
+                    diff.wrapped
+                );
+                write(config_str, path)?;
+            }
+
+            Ok(config)
+        } else {
+            warn!("Config `{path:?}` not found, writing default config");
+            // Config missing, create and return default config at path
+            let config = UrsaConfig::default();
+            write(toml::to_string(&config)?, path)?;
+
+            Ok(config)
+        }
+    }
+}
+
+pub fn write<S: Display, P: Into<PathBuf>>(str: S, path: P) -> Result<()> {
+    let path = path.into();
+    create_dir_all(path.parent().unwrap())?;
+    let mut file = File::create(path)?;
+    file.write_all(str.to_string().as_bytes())?;
+    Ok(())
 }
