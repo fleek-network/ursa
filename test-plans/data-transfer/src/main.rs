1c6464c9a7825dbf4cf5f125b682ce6eedb8b5a3
Miguel Guarniz <miguel@fleek.xyz>
2023-01-06 15:04:10 -0800

    feat: testplan data transfer (#267)
    * Add validation of peer count after discovery
    
    * Modularize network init logic
    
    * Add max number of tries for discovery
    
    * Clean up
    
    * Move bootstrap stuff to separate mod
    
    * Clean up
    
    * Rename test plan to replication
    
    * Fix fmt
    
    * Update README
    
    * Rename testplan to data-transfer
    
    * Rename starter functions

---
+++ test-plans/data-transfer/src/main.rs
@@ -0,0 +1,23 @@
+mod bootstrap;
+
+use env_logger::Env;
+use testground::client::Client;
+
+#[tokio::main]
+async fn main() {
+    let mut client = Client::new_and_init().await.unwrap();
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    // The first instance that arrives will be the bootstrapper.
+    let seq = client.global_seq();
+    if seq == 1 {
+        return bootstrap::start_bootstrap(client).await;
+    }
+
+    if let Err(e) = bootstrap::start_node(&mut client).await {
+        client.record_failure(e).await.expect("Success");
+    } else {
+        client.record_success().await.expect("Success");
+    }
+}
