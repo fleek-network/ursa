a7cfd35647ba4a3c7563a784870b8a806298bbc1
Matthias Wright <matthias.s.wright@gmail.com>
2023-01-11 23:33:11 +0100

    feat: refined bootstrapping for testplan (#282)
    * feat: refine bootstrapping strategy for testplans
    
    * change number of instances
    
    * chore: remove comment
    
    * Update test-plans/data-transfer/src/bootstrap.rs
    
    Co-authored-by: Miguel Guarniz <miguel@fleek.xyz>
    
    Co-authored-by: Miguel Guarniz <miguel@fleek.xyz>

--- test-plans/data-transfer/src/bootstrap.rs
+++ test-plans/data-transfer/src/bootstrap.rs
@@ -1,6 +1,7 @@
 use db::MemoryDB;
 use futures::StreamExt;
 use libp2p::PeerId;
+use rand::Rng;
 use std::borrow::Cow;
 use std::collections::HashSet;
 use std::sync::Arc;
@@ -8,11 +9,14 @@ use testground::client::Client;
 use ursa_network::{NetworkCommand, NetworkConfig, UrsaService};
 use ursa_store::UrsaStore;
 
+static NODES_PER_BATCH: u64 = 5;
+
 fn node_init(keypair: libp2p::identity::Keypair, config: NetworkConfig) -> UrsaService<MemoryDB> {
     let db = Arc::new(MemoryDB::default());
     let store = Arc::new(UrsaStore::new(Arc::clone(&db)));
     UrsaService::new(keypair, &config, Arc::clone(&store)).unwrap()
 }
+
 
 pub async fn start_bootstrap(client: testground::client::Client) {
     let local_key = libp2p::identity::Keypair::generate_ed25519();
@@ -65,6 +69,7 @@ pub async fn start_bootstrap(client: testground::client::Client) {
 pub async fn start_node(client: &mut Client) -> Result<(), String> {
     let seq = client.global_seq();
     let test_instance_count = client.run_parameters().test_instance_count as usize;
+    let num_nodes = client.run_parameters().test_instance_count - 1;
 
     let bootstrap_addr = client
         .subscribe("bootstrap-addr", test_instance_count)
@@ -93,14 +98,38 @@ pub async fn start_node(client: &mut Client) -> Result<(), String> {
     config.bootstrap_nodes = vec![bootstrap_addr.parse().unwrap()];
     config.swarm_addrs = vec![local_addr.parse().unwrap()];
 
-    // Wait until bootstrapping is done.
+    let mut batch_index = 0;
+    // Divide the nodes in batches of size `NODES_PER_BATCH`.
+    // The last batch may have less than `NODES_PER_BATCH` nodes.
+    let num_batches = ((num_nodes as f64) / (NODES_PER_BATCH as f64)).ceil() as u64;
+    // Start iteration at 1 so that the nodes in the first batch don't run into the barrier.
+    // If `num_nodes` is less than `NODES_PER_BATCH`, `num_batches` will be 1 and the loop
+    // is never entered, so nodes don't run into a barrier in that case
+    for i in 1..num_batches {
+        let lower = i * NODES_PER_BATCH + 1;
+        let upper = if i == num_batches - 1 {
+            num_nodes
+        } else {
+            i * NODES_PER_BATCH + NODES_PER_BATCH
+        };
+        if seq >= lower && seq <= upper {
+            batch_index = i;
+            
+            // Wait for previous batch to finish
+            client.barrier(format!("batch-{}-done", batch_index - 1), NODES_PER_BATCH).await.unwrap();
+        }
+    }
+
+    // Wait for bootstrapping node.
     client.barrier("bootstrap-ready", 1).await.unwrap();
 
     // Each peer should wait before attempting to bootstrap.
-    // If all peers attempt to bootstrap at the same time, DHT won't
+    // If all peers in the batch attempt to bootstrap at the same time, DHT won't
     // be updated in time for peers to discover each other.
     // We have no way to trigger bootstrapping later.
-    tokio::time::sleep(tokio::time::Duration::from_secs(seq * 4)).await;
+    let mut rng = rand::thread_rng();
+    let interval = rng.gen_range(1..NODES_PER_BATCH * 4);
+    tokio::time::sleep(tokio::time::Duration::from_secs(interval)).await;
     client.record_message(format!(
         "[Node]: Bootstrapping to address {}",
         bootstrap_addr
@@ -132,12 +161,16 @@ pub async fn start_node(client: &mut Client) -> Result<(), String> {
         cmd_sender.send(msg).unwrap();
         peers = peers_receiver.await.expect("Success");
 
-        client.record_message(format!("[Node]: Peers: {peers:?}"));
+        client.record_message(format!("[Node]: Node {} has {} peers", seq, peers.len()));
     }
 
-    client.signal("done").await.unwrap();
+    // Once all nodes in a batch have reached this point, the nodes in the next batch
+    // are notified and can continue.
+    client.signal(format!("batch-{}-done", batch_index)).await.unwrap();
+    // All nodes wait here and signal to the bootstrap node that they are done.
+    client.signal_and_wait("done", num_nodes).await.unwrap();
 
-    if peers.len() == peer_count {
+    if peers.len() > 1 {
         Ok(())
     } else {
         Err("failed to bootstrap".to_string())
