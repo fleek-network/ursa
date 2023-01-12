4078788da5909581ebaac7fb8f9edd454125c8f0
Miguel <miguel@fleek.xyz>
2023-01-12 15:34:32 -0500

    Store UrsaStore instead of graphsync or bitswap storage

--- test-plans/data-transfer/src/node.rs
+++ test-plans/data-transfer/src/node.rs
@@ -2,17 +2,17 @@ use db::MemoryDB;
 use std::sync::Arc;
 use tokio::sync::mpsc::UnboundedSender as Sender;
 use ursa_network::NetworkCommand;
-use ursa_store::{GraphSyncStorage, UrsaStore};
+use ursa_store::UrsaStore;
 
 pub struct Node {
-    pub store: GraphSyncStorage<MemoryDB>,
+    pub store: Arc<UrsaStore<MemoryDB>>,
     pub command_sender: Sender<NetworkCommand>,
 }
 
 impl Node {
     pub fn new(store: Arc<UrsaStore<MemoryDB>>, command_sender: Sender<NetworkCommand>) -> Self {
         Self {
-            store: GraphSyncStorage(store),
+            store,
             command_sender,
         }
     }
