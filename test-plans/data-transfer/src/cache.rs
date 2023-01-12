572c5324fa7e6f36cf2f3ae8cda5b2954c734ca1
Miguel <miguel@fleek.xyz>
2023-01-12 13:19:06 -0500

    Add noop cache request test

--- test-plans/data-transfer/src/cache.rs
+++ test-plans/data-transfer/src/cache.rs
@@ -1,29 +1,6 @@
 use crate::node::Node;
-use cid::multihash::Code;
-use ipld_traversal::blockstore::Blockstore;
-use libipld::{cbor::DagCborCodec, ipld, Block, DefaultParams, Ipld};
 use testground::client::Client;
-use tokio::sync::oneshot;
-use ursa_network::NetworkCommand;
-
-fn get_block(content: &[u8]) -> Block<DefaultParams> {
-    create_block(ipld!(content))
-}
-
-fn create_block(ipld: Ipld) -> Block<DefaultParams> {
-    Block::encode(DagCborCodec, Code::Blake3_256, &ipld).unwrap()
-}
 
 pub async fn test_cache_request(_client: &mut Client, _node: Node) -> Result<(), String> {
-    // let block = get_block(&b"hello world"[..]);
-    // node.store.insert(&block).unwrap();
-    // assert!(node.store.has(block.cid()).unwrap());
-    //
-    // let (sender, _) = oneshot::channel();
-    // let _request = NetworkCommand::Put {
-    //     cid: *block.cid(),
-    //     sender,
-    // };
-
     Ok(())
 }
