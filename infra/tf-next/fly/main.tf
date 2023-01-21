04a0f2ac0e34aed05c2b377ceae58188f4884240
b0xtch <botchm@hotmail.com>
2023-01-21 05:33:45 -0800

    add install ca certs to docker image, increase volume of gateway node

--- infra/tf-next/fly/main.tf
+++ infra/tf-next/fly/main.tf
@@ -16,14 +16,14 @@ resource "fly_app" "ursa_gateway" {
   org  = "fleek-network"
 }
 
-# resource "fly_volume" "ursa_gateway_volume" {
-#   app    = fly_app.ursa_gateway.name
-#   name   = "ursa_gateway_volume"
-#   size   = 15
-#   region = "yyz"
-
-#   depends_on = [fly_app.ursa_gateway]
-# }
+resource "fly_volume" "ursa_gateway_volume" {
+  for_each = toset(var.regions)
+  app = fly_app.ursa_gateway.name
+  region = each.key
+  name = "ursa_data_${each.key}"
+  size =  15
+  depends_on = [fly_app.ursa_gateway]
+}
 
 resource "fly_ip" "ursa_gateway_ip" {
   app        = fly_app.ursa_gateway.name
