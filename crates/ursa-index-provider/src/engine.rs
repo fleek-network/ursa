d45eaf2cb597985b44fcdfb364c3d8cefe2ac475
ozwaldorf <self@ossian.dev>
2023-01-24 00:32:48 -0500

    chore: domain migration to `server_config` from `provider_config`

--- crates/ursa-index-provider/src/engine.rs
+++ crates/ursa-index-provider/src/engine.rs
@@ -93,6 +93,7 @@ pub struct ProviderEngine<S> {
     network_command_sender: Sender<NetworkCommand>,
     /// Server from which advertised content is retrievable.
     server_address: Multiaddr,
+    domain: Multiaddr,
 }
 
 impl<S> ProviderEngine<S>
@@ -106,6 +107,7 @@ where
         config: ProviderConfig,
         network_command_sender: Sender<NetworkCommand>,
         server_address: Multiaddr,
+        domain: Multiaddr,
     ) -> Self {
         let (command_sender, command_receiver) = unbounded_channel();
         ProviderEngine {
@@ -116,6 +118,7 @@ where
             provider: Provider::new(keypair, provider_store),
             store,
             server_address,
+            domain,
         }
     }
     pub fn command_sender(&self) -> Sender<ProviderCommand> {
@@ -163,7 +166,7 @@ where
                         } else {
                             match self
                                 .provider
-                                .create_announce_message(peer_id, self.config.domain.clone())
+                                .create_announce_message(peer_id, self.domain.clone())
                             {
                                 Ok(announce_message) => {
                                     if let Err(e) = self
