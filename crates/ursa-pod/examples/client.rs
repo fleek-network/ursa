e816931e73de7658937ae0354cf8ed785ff9b8f6
ozwaldorf <self@ossian.dev>
2023-03-28 23:49:21 -0400

    refactor: example now exchanges a handshake

--- crates/ursa-pod/examples/client.rs
+++ crates/ursa-pod/examples/client.rs
@@ -1,49 +1,57 @@
 use std::error::Error;
-//use std::net::ToSocketAddrs;
 
-use bytes::BytesMut;
-use tokio::io::AsyncWriteExt;
-use tokio::net::TcpStream;
-use tokio::net::ToSocketAddrs;
-use tokio_util::codec::Encoder;
-use ursa_pod::codec::UrsaCodec;
-use ursa_pod::codec::UrsaFrame;
+use futures::SinkExt;
+use tokio::net::{TcpStream, ToSocketAddrs};
+use tokio_stream::StreamExt;
+use tokio_util::codec::Framed;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+use ursa_pod::codec::{UrsaCodec, UrsaFrame};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
     UfdpClient::new("127.0.0.1:8080").await?;
     Ok(())
 }
 
 pub struct UfdpClient {
-    _transport: TcpStream,
-    _codec: UrsaCodec,
+    _transport: Framed<TcpStream, UrsaCodec>,
 }
 
 impl UfdpClient {
     pub async fn new<T: ToSocketAddrs>(dest: T) -> Result<Self, Box<dyn Error>> {
-        let mut codec = UrsaCodec::default();
-        let mut transport = TcpStream::connect(dest).await?;
+        let codec = UrsaCodec::default();
+        let stream = TcpStream::connect(dest).await?;
+        let mut transport = Framed::new(stream, codec);
 
         // send handshake
-        let mut buf = BytesMut::new();
-        codec
-            .encode(
-                UrsaFrame::HandshakeRequest {
-                    version: 0,
-                    supported_compression_bitmap: 0,
-                    lane: 0xFF,
-                    pubkey: [1; 48],
-                },
-                &mut buf,
-            )
-            .unwrap();
-        transport.writable().await?;
-        transport.write_all(&buf).await?;
+        transport
+            .send(UrsaFrame::HandshakeRequest {
+                version: 0,
+                supported_compression_bitmap: 0,
+                lane: 0xFF,
+                pubkey: [1; 48],
+            })
+            .await
+            .expect("handshake request");
+
+        // receive handshake
+        if let Ok(frame) = transport.next().await.expect("handshake response") {
+            match frame {
+                UrsaFrame::HandshakeResponse { .. } => {
+                    info!("received handshake response from server: {frame:?}");
+                }
+                _ => panic!("unexpected frame"),
+            }
+        }
 
         Ok(Self {
             _transport: transport,
-            _codec: codec,
         })
     }
 }
--- crates/ursa-pod/examples/server.rs
+++ crates/ursa-pod/examples/server.rs
@@ -1,10 +1,12 @@
 use std::{error::Error, fmt::Display};
 
+use futures::SinkExt;
 use tokio::net::{TcpListener, ToSocketAddrs};
 use tokio_stream::StreamExt;
 use tokio_util::codec::Framed;
-use tracing::info;
-use ursa_pod::codec::UrsaCodec;
+use tracing::{error, info, Level};
+use tracing_subscriber::FmtSubscriber;
+use ursa_pod::codec::{UrsaCodec, UrsaFrame};
 
 struct UfdpServer {
     listener: TcpListener,
@@ -25,7 +27,22 @@ impl UfdpServer {
                 let mut transport = Framed::new(stream, UrsaCodec::default());
 
                 while let Some(request) = transport.next().await {
-                    println!("{request:?}");
+                    info!("{request:?}");
+                    match request {
+                        Ok(UrsaFrame::HandshakeRequest { lane, .. }) => {
+                            transport
+                                .send(UrsaFrame::HandshakeResponse {
+                                    pubkey: [2; 33],
+                                    epoch_nonce: 1000,
+                                    lane: if lane == 0xFF { 0 } else { lane },
+                                    last: None,
+                                })
+                                .await
+                                .expect("handshake response");
+                        }
+                        Ok(_) => {}
+                        Err(e) => error!("{e:?}"),
+                    }
                 }
             });
         }
@@ -34,6 +51,11 @@ impl UfdpServer {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
     let server = UfdpServer::new("0.0.0.0:8080").await?;
     server.start().await
 }
