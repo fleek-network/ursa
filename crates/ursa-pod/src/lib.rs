3acaa411c913ba687f3a87cbdc5f5186de7ff79a
Parsa <i@parsa.ooo>
2023-04-11 15:20:14 -0400

    chore: pod tree (#492)
    * implement TreeWalker
    
    * implement ProofEncoder
    
    * implement ProofBuf
    
    * implement IncrementalTree
    
    * fix the calc of should_flip
    
    * wip: incremental verifier
    
    * implement walker::Resume and test the walker
    
    * consistent sign bit order on the encoder
    
    * write better test for incremental verifier
    
    * explain the unsafe blocks
    
    * test verification on resume mode
    
    * write more test and implement the is_done state
    
    * add test for range request + resume
    
    * make incremental verifier return error instead of panics
    
    * add more test and fix a bug in walker and verify
    
    * fuzz the implementation
    
    * add test to detect memory leak
    
    * fix doc
    
    * benchmark the tree
    
    * s/an/a
    
    Co-authored-by: Mahmoud <botchm@hotmail.com>
    
    ---------
    
    Co-authored-by: Mahmoud <botchm@hotmail.com>

--- crates/ursa-pod/src/lib.rs
+++ crates/ursa-pod/src/lib.rs
@@ -4,6 +4,7 @@
 pub mod codec;
 pub mod keys;
 pub mod primitives;
+pub mod tree;
 /// UFDP types
 pub mod types;
 
@@ -13,3 +14,6 @@ pub mod client;
 /// UFDP server implementation
 #[cfg(feature = "server")]
 pub mod server;
+
+/// Reexport of the Blake3 we use.
+pub use blake3;
