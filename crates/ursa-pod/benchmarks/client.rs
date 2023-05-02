225a985184961931fc017269bfa86ad62964d4eb
ozwaldorf <self@ossian.dev>
2023-05-02 14:55:51 -0400

    refactor: bin benches

--- crates/ursa-pod/benchmarks/client.rs
+++ crates/ursa-pod/benchmarks/client.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     env,
     process::exit,
     str::FromStr,
@@ -7,14 +8,86 @@ use std::{
 
 use futures::{future::Either, stream::FuturesUnordered, StreamExt};
 use tokio::net::TcpStream;
-use ursa_pod::{client::UfdpClient, connection::UrsaCodecError, types::Blake3Cid};
+use ursa_pod::{blake3::Hash, client::UfdpClient, connection::UrsaCodecError};
 
 const PUB_KEY: [u8; 48] = [2u8; 48];
+const SIZES: [(&str, u64); 17] = [
+    // MB
+    (
+        "488de202f73bd976de4e7048f4e1f39a776d86d582b7348ff53bf432b987fca8",
+        1024 * 1024,
+    ),
+    (
+        "8ac83f8ce09d064b023ab3c15880b02f2686cd1817fd25915b8153316ee059f8",
+        2 * 1024 * 1024,
+    ),
+    (
+        "04e52cd2da6a0e1f338b0078369130d96585c1de65057da5dd1283b12fb853e1",
+        4 * 1024 * 1024,
+    ),
+    (
+        "27ddc0a1d824fa6befe0596ddc0136fc4a1dc060d526800851f87498768b755c",
+        8 * 1024 * 1024,
+    ),
+    (
+        "b4834959bc889fed1abf3c45d5da0e384134386a4b2786cc5dbb9fe8fa853bbb",
+        16 * 1024 * 1024,
+    ),
+    (
+        "2dbe083a48e7430772c9803d474523a53b7c8aad5c7daaba0a55b6f892a98074",
+        32 * 1024 * 1024,
+    ),
+    (
+        "ea7b156fc9a810c181984f9e2da433feeeb2bf88ffa4d1f0dc1a92154b5bdc8b",
+        64 * 1024 * 1024,
+    ),
+    (
+        "e66d34ca5a36dfa692903a66d66fd1d9c87bd553c32fbf7d3960542f4cda3257",
+        128 * 1024 * 1024,
+    ),
+    (
+        "9216a60cba88b32b18349b83c57c22d2e3b514a9720916952e214e5fc065c538",
+        256 * 1024 * 1024,
+    ),
+    (
+        "34f2f34bcc048af98242e010b4a661348276a784d9f9f99fcff70bf94fe8b9ba",
+        512 * 1024 * 1024,
+    ),
+    // GB
+    (
+        "94b4ec39d8d42ebda685fbb5429e8ab0086e65245e750142c1eea36a26abc24d",
+        1024 * 1024 * 1024,
+    ),
+    (
+        "cbd71ef31685ea2c6ce0c146ef1d160b4d458f29cea2a61536a8a65f195fdb82",
+        2 * 1024 * 1024 * 1024,
+    ),
+    (
+        "7dde7c9fed144013fedbe2b0bbf2d82f004b60b589485851cdec29b27be408d7",
+        4 * 1024 * 1024 * 1024,
+    ),
+    (
+        "875283713208b0d6be59b2c6862b0a3cfdd8ebe5366b815e34dfffd98554ef26",
+        8 * 1024 * 1024 * 1024,
+    ),
+    (
+        "7953bbc0a374e96ea2caa5328ebd96f3545b41c882f509e92ae850d000f3cde3",
+        16 * 1024 * 1024 * 1024,
+    ),
+    (
+        "92705fed65135cde7f3c9901c801e062f8483bdd6dd88f167858cbf47644eab1",
+        32 * 1024 * 1024 * 1024,
+    ),
+    (
+        "20e6c9ca2d7ed61951ea515e5b82bc478fbe6b7edb051272360c3b7494a3dd27",
+        64 * 1024 * 1024 * 1024,
+    ),
+];
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), UrsaCodecError> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 6 {
+    if args.len() != 5 {
         help(&args[0]);
         exit(1)
     }
@@ -22,20 +95,23 @@ async fn main() -> Result<(), UrsaCodecError> {
     let seconds = u64::from_str(&args[2]).expect("parse number of seconds to run new requests for");
     let duration = Duration::from_secs(seconds);
     let workers = u64::from_str(&args[3]).expect("parse number of concurrent requests");
-    let block_size = u64::from_str(&args[4]).expect("parse block size");
-    let file_size = u64::from_str(&args[5]).expect("parse file size");
+    let file_size = u64::from_str(&args[4]).expect("parse file size");
 
-    let mut cid = [0u8; 32];
-    cid[0..8].copy_from_slice(&block_size.to_be_bytes());
-    cid[8..16].copy_from_slice(&file_size.to_be_bytes());
-    let cid = Blake3Cid(cid);
+    let hashes: HashMap<u64, Hash> = SIZES
+        .iter()
+        .map(|(h, s)| (*s, Hash::from_hex(h).unwrap()))
+        .collect();
 
-    println!("warming up for ~3 seconds with {workers} workers");
+    let cid = *hashes
+        .get(&file_size)
+        .expect("Hash not found for content size");
+
+    println!("Warming up for ~3 seconds with {workers} workers");
     let (elapsed, total) = run(cid, address, workers, Either::Right(Duration::from_secs(3))).await;
     let avg = elapsed.as_secs_f64() / total as f64;
     let estimated = (duration.as_secs_f64() / avg) as u64;
 
-    println!("Running {estimated} requests with {workers} workers for a target {duration:?}");
+    println!("Running {estimated} requests with {workers} workers for a target {duration:?}\n");
     let (elapsed, total) = run(cid, address, workers, Either::Left(estimated)).await;
     println!("\u{001b}[1mTotal\u{001b}[0m: {elapsed:?}");
     let secs = elapsed.as_secs_f64();
@@ -47,12 +123,7 @@ async fn main() -> Result<(), UrsaCodecError> {
     Ok(())
 }
 
-async fn run(
-    cid: Blake3Cid,
-    addr: &str,
-    workers: u64,
-    param: Either<u64, Duration>,
-) -> (Duration, u64) {
+async fn run(cid: Hash, addr: &str, workers: u64, param: Either<u64, Duration>) -> (Duration, u64) {
     let mut futures = FuturesUnordered::new();
     let mut total = workers;
 
@@ -82,12 +153,12 @@ async fn run(
     (instant.elapsed(), total)
 }
 
-async fn request(cid: Blake3Cid, addr: &str) -> usize {
+async fn request(cid: Hash, addr: &str) -> usize {
     let stream = TcpStream::connect(addr).await.unwrap();
     let mut client = UfdpClient::new(stream, PUB_KEY, None).await.unwrap();
     client.request(cid).await.unwrap()
 }
 
 fn help(bin: &str) {
-    println!("USAGE: {bin} <server ip> <duration> <workers> <file size> <block size>");
+    println!("USAGE: {bin} <server ip> <duration> <workers> <file size>");
 }
--- crates/ursa-pod/benchmarks/server.rs
+++ crates/ursa-pod/benchmarks/server.rs
@@ -1,54 +1,99 @@
-use std::cmp::min;
+use std::{
+    cmp::min,
+    collections::{BTreeMap, HashMap},
+    io::Write,
+};
 
 use arrayref::array_ref;
 use tokio::net::TcpListener;
 use ursa_pod::{
+    blake3::Hash,
     instrument,
     server::{Backend, UfdpHandler},
-    types::{Blake3Cid, BlsSignature, Secp256k1PublicKey},
+    types::{BlsSignature, Secp256k1PublicKey},
 };
 
 const ADDRESS: &str = "0.0.0.0:6969";
-// max length a block can be
-const BLOCK: &[u8] = &[0; 256 * 1024 * 1024];
+// content for a block
+const BLOCK: &[u8] = &[0; 256 * 1024];
+const SIZES: [usize; 17] = [
+    // MB
+    1 * 1024 * 1024,
+    2 * 1024 * 1024,
+    4 * 1024 * 1024,
+    8 * 1024 * 1024,
+    16 * 1024 * 1024,
+    32 * 1024 * 1024,
+    64 * 1024 * 1024,
+    128 * 1024 * 1024,
+    256 * 1024 * 1024,
+    512 * 1024 * 1024,
+    // GB
+    1 * 1024 * 1024 * 1024,
+    2 * 1024 * 1024 * 1024,
+    4 * 1024 * 1024 * 1024,
+    8 * 1024 * 1024 * 1024,
+    16 * 1024 * 1024 * 1024,
+    32 * 1024 * 1024 * 1024,
+    64 * 1024 * 1024 * 1024,
+];
 
-#[derive(Clone, Copy)]
-struct BenchmarkBackend {}
+#[derive(Clone, Debug)]
+struct BenchmarkBackend {
+    sizes: HashMap<Hash, usize>,
+    trees: HashMap<Hash, Vec<[u8; 32]>>,
+}
 
-impl Backend for BenchmarkBackend {
-    fn raw_block(&self, cid: &Blake3Cid, block: u64) -> Option<&[u8]> {
-        // for benchmarking, we'll determine how much data to send from the hash.
-        // The first 8 bytes are the block size, and the next 8 are the content size
-        let block_size_bytes = array_ref!(cid.0, 0, 8);
-        let block_size = min(u64::from_be_bytes(*block_size_bytes), BLOCK.len() as u64);
-        let file_size_bytes = array_ref!(cid.0, 8, 8);
-        let file_size = u64::from_be_bytes(*file_size_bytes);
+impl BenchmarkBackend {
+    fn new() -> Self {
+        let mut sizes = HashMap::new();
+        let mut trees = HashMap::new();
 
-        let block_max = (file_size + block_size - 1) / block_size;
-
-        if block < block_max {
-            // return remainder if it's the last block and the size is less than 1 block
-            let rem = file_size % block_size;
-            let take = if block + 1 == block_max && rem != 0 {
-                rem
-            } else {
-                block_size
-            };
-
-            if take != 0 {
-                Some(&BLOCK[0..take as usize])
-            } else {
-                None
+        for (i, size) in SIZES.iter().enumerate() {
+            eprint!("\rBuilding blake3 trees ... ({i}/{})", SIZES.len() - 1);
+            std::io::stdout().flush().unwrap();
+            let mut tree_builder = blake3::ursa::HashTreeBuilder::new();
+            let mut i = 0;
+            while let Some(block) = Self::raw_block(*size, i) {
+                tree_builder.update(block);
+                i += 1
             }
+            let output = tree_builder.finalize();
+            sizes.insert(output.hash, *size);
+            trees.insert(output.hash, output.tree);
+        }
+        eprintln!("\rBuilding blake3 trees ... (done)   ");
+        let mut arr = sizes.iter().collect::<Vec<(&Hash, &usize)>>();
+        arr.sort_by(|(_, a), (_, b)| a.cmp(b));
+        eprintln!("{arr:?}");
+
+        Self { sizes, trees }
+    }
+
+    fn raw_block<'a>(len: usize, block: usize) -> Option<&'static [u8]> {
+        let s = block * BLOCK.len();
+        if s < len {
+            let e = len.min(s + BLOCK.len());
+            Some(&BLOCK[..e - s])
         } else {
             None
         }
     }
+}
+
+impl Backend for BenchmarkBackend {
+    fn raw_block(&self, hash: &Hash, block: u64) -> Option<&[u8]> {
+        self.sizes
+            .get(hash)
+            .and_then(|len| Self::raw_block(*len, block as usize))
+    }
+
+    fn get_tree(&self, cid: &Hash) -> Option<Vec<[u8; 32]>> {
+        self.trees.get(cid).cloned()
+    }
 
     fn decryption_key(&self, _request_id: u64) -> (ursa_pod::types::Secp256k1AffinePoint, u64) {
-        let key = [1; 33];
-        let key_id = 0;
-        (key, key_id)
+        ([3u8; 33], 0)
     }
 
     fn get_balance(&self, _pubkey: Secp256k1PublicKey) -> u128 {
@@ -60,16 +105,20 @@ impl Backend for BenchmarkBackend {
     }
 }
 
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() {
-    let listener = TcpListener::bind(ADDRESS).await.unwrap();
-    let mut session_id = 0;
+    let backend = BenchmarkBackend::new();
 
+    let listener = TcpListener::bind(ADDRESS).await.unwrap();
+    eprintln!("Listening on {ADDRESS}");
+
+    let mut session_id = 0;
     loop {
         let (stream, _) = listener.accept().await.unwrap();
 
+        let backend = backend.clone();
         tokio::spawn(async move {
-            let handler = UfdpHandler::new(stream, BenchmarkBackend {}, session_id);
+            let handler = UfdpHandler::new(stream, backend, session_id);
             instrument!(
                 handler.serve().await.unwrap(),
                 "sid={session_id},tag=session"
